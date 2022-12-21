c21fe484f105477831fc317139669adced5e0414
pujiragil <ragilsg164@gmail.com>
2022-12-21 17:15:40 +0700

    create switch button theme

---
+++ components/Switch/index.tsx
@@ -0,0 +1,7 @@
+export default function Switch() {
+  return (
+    <button className="outline-none border-none p-3 rounded-full cursor-pointer bg-switch-light dark:bg-switch-dark">
+      <img className="w-6 h-6" src="/dark.svg" alt="dark-button" />
+    </button>
+  );
+}
