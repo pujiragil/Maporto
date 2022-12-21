c22fda952e935849dd5fec18e5299b718d5fc6b0
pujiragil <ragilsg164@gmail.com>
2022-12-21 17:19:26 +0700

    new top section components

---
+++ components/Profile/index.tsx
@@ -0,0 +1,9 @@
+import Switch from "../Switch";
+
+export default function Profile() {
+  return (
+    <div className="flex flex-col gap-5 items-center justify-center">
+      <Switch/>
+    </div>
+  )
+}
