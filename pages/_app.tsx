f51b325bfd8d9e93d371afebe4d5c87f0cd29cc2
pujiragil <ragilsg164@gmail.com>
2022-12-21 16:36:47 +0700

    add custom color configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import '../styles/globals.css'
+import { Poppins } from '@next/font/google'
 import type { AppProps } from 'next/app'
 
+const poppins = Poppins({
+  weight: "500",
+  subsets: ["latin"]
+})
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={poppins.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,29 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "media",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light": "#E9EBEC",
+        "dark": "#0C151D",
+        "primary": {
+          "one": "#0C151D",
+          "two": "#575757",
+          "three": "#3D3D3D",
+          "four": "#19191B"
+        },
+        "secondary": {
+          "one": "#F1F2F4",
+          "two": "#A3ABB2",
+          "three": "#3D3D3D"
+        },
+        "orange": {
+          "dark": "#FFE071",
+          "light": "#FBD144"
+        }
+      }
+    },
   },
   plugins: [],
 }
