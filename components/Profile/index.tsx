11f9eda95343448e34efb966e98e731aeb4481c0
pujiragil <ragilsg164@gmail.com>
2022-12-22 14:48:09 +0700

    change md responsive size

--- components/Profile/index.tsx
+++ components/Profile/index.tsx
@@ -3,17 +3,18 @@ import Switch from "../Switch";
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
 import { FaTelegram } from "react-icons/fa";
 import Link from "next/link";
-import About from "../About";
+import MainButton from "../Button";
 
 export default function Profile() {
   return (
-    <div className="flex flex-col gap-8 md:gap-16 items-center justify-center">
+    <div className="flex flex-col gap-8 md:gap-12 xl:gap-16 items-center justify-center">
       <Switch />
       <div className="flex flex-col md:flex-row gap-10 items-center justify-center">
         <Avatar />
         <Information />
       </div>
-      <About />
+      <Experience/>
+      <MainButton/>
     </div>
   );
 }
@@ -22,10 +23,10 @@ const Information = () => {
   return (
     <div className="flex flex-col gap-5">
       <div className="text-center md:text-left">
-        <h1 className="text-primary-one dark:text-secondary-one text-head mb-2">
+        <h1 className="text-primary-one dark:text-secondary-one text-head mb-4">
           Puji Ragil Setyo G
         </h1>
-        <p className="text-primary-two dark:text-secondary-two text-sm">
+        <p className="text-primary-two dark:text-secondary-two text-sm md:text-xl">
           Frontend Engineer
         </p>
       </div>
@@ -52,3 +53,22 @@ const Social = () => {
     </div>
   );
 };
+
+const Experience = () => {
+  return (
+    <div className="p-5 flex justify-center gap-3 md:gap-14 text-primary-two dark:text-secondary-two">
+      <div className="flex flex-1 flex-col text-center max-w-[105px] md:max-w-[125px]">
+        <p className="md:text-2xl">1+</p>
+        <p className="text-sm md:text-xl">Years of experience</p>
+      </div>
+      <div className="flex flex-1 flex-col text-center max-w-[105px] md:max-w-[125px]">
+        <p className="md:text-2xl">8+</p>
+        <p className="text-sm md:text-xl">Personal side project</p>
+      </div>
+      <div className="flex flex-1 flex-col text-center max-w-[105px] md:max-w-[125px]">
+        <p className="md:text-2xl">0</p>
+        <p className="text-sm md:text-xl">Client side project</p>
+      </div>
+    </div>
+  )
+}
