9d06e73fe3e192cc5f5ff1111c4f76407c0ab5c9
pujiragil <ragilsg164@gmail.com>
2022-12-22 08:29:41 +0700

    fix next-themes render issue

--- components/Switch/index.tsx
+++ components/Switch/index.tsx
@@ -1,7 +1,18 @@
 import { useTheme } from "next-themes";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Switch() {
+  const [mounted, setMounted] = useState(false)
   const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if(!mounted) {
+    return null
+  }
 
   const handleTheme = () => {
     setTheme(theme === "dark" ? "light" : "dark");
@@ -12,11 +23,26 @@ export default function Switch() {
       onClick={handleTheme}
       className="outline-none border-none p-3 rounded-full cursor-pointer bg-switch-light dark:bg-switch-dark hover:bg-secondary-two hover:dark:bg-primary-two"
     >
-      {theme === "dark" ? (
-        <img className="w-6 h-6" src="/dark.svg" alt="dark-button" />
-      ) : (
-        <img className="w-6 h-6" src="/light.svg" alt="light-button" />
-      )}
+      <ThemedImage/>
     </button>
   );
+}
+
+function ThemedImage() {
+  const { resolvedTheme } = useTheme()
+  let src
+
+  switch (resolvedTheme) {
+    case 'light':
+      src = '/light.svg'
+      break
+    case 'dark':
+      src = '/dark.svg'
+      break
+    default:
+      src = '/dark.svg'
+      break
+  }
+
+  return <Image src={src} width={20} height={20} alt="icon" />
 }
