159e391755ac49226d734aaf4d64a332dbc41b09
pujiragil <ragilsg164@gmail.com>
2022-12-22 09:34:40 +0700

    create about section

---
+++ components/About/index.tsx
@@ -0,0 +1,13 @@
+export default function About() {
+  return (
+    <div className="flex flex-col gap-2 text-center p-5">
+      <h2 className="text-xl text-primary-two font-semibold dark:text-secondary-two">
+        About Me
+      </h2>
+      <p className="text-sm text-primary-two dark:text-secondary-two">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis nam
+        exercitationem illo!
+      </p>
+    </div>
+  );
+}
