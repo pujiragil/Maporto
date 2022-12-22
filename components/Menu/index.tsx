7325ffc9fc5205a2dea89b67734d90fdcc3e966e
pujiragil <ragilsg164@gmail.com>
2022-12-22 22:05:44 +0700

    styling portfolio section for mobile and tablet

--- components/Menu/index.tsx
+++ components/Menu/index.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import Portfolio from "../Portfolio";
 
 export default function Menu() {
   return (
     <div className="py-2.5 flex flex-col items-center justify-center gap-8">
       <MenuSwitch />
+      <Portfolio />
     </div>
   );
 }
@@ -16,7 +18,7 @@ function MenuSwitch() {
   };
 
   return (
-    <div className="w-full md:w-8/12 lg:w-6/12 flex items-center justify-center p-2 gap-3.5 md:gap-7 bg-switch-light dark:bg-switch-dark rounded-xl text-xs md:text-sm lg:text-base">
+    <div className="w-full md:w-9/12 lg:w-7/12 flex items-center justify-center p-2 gap-3.5 md:gap-7 bg-switch-light dark:bg-switch-dark rounded-xl text-xs md:text-sm lg:text-base">
       <button
         onClick={handleActive}
         name="portfolio"
--- components/Portfolio/index.tsx
+++ components/Portfolio/index.tsx
@@ -1,3 +1,25 @@
 export default function Portfolio() {
-  
+  return (
+    <div className="grid md:grid-cols-2 w-full md:w-9/12 lg:w-7/12 gap-8">
+      <PortfolioItem src="/images/port-1.png" alt="porfolio-image" />
+      <PortfolioItem src="/images/port-2.png" alt="porfolio-image" />
+      <PortfolioItem src="/images/port-3.png" alt="porfolio-image" />
+      <PortfolioItem src="/images/port-4.png" alt="porfolio-image" />
+      <PortfolioItem src="/images/port-5.png" alt="porfolio-image" />
+      <PortfolioItem src="/images/port-6.png" alt="porfolio-image" />
+    </div>
+  );
+}
+
+interface PortfolioItemProps {
+  src: string;
+  alt: string;
+}
+
+function PortfolioItem({ src, alt }: PortfolioItemProps) {
+  return (
+    <div className="w-full h-52 overflow-hidden rounded-2xl">
+      <img className="w-full h-full object-cover" src={src} alt={alt} />
+    </div>
+  );
 }
