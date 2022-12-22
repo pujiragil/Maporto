00ee03537e84cda8bad19e94e1f495144b7732a0
pujiragil <ragilsg164@gmail.com>
2022-12-22 17:48:01 +0700

    new style for md size(tablet)

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,11 +1,11 @@
 export default function MainButton() {
   return (
-    <div className="w-full flex items-center justify-center gap-3.5 py-5">
-      <button className="flex-1 flex gap-2 items-center justify-center bg-orange-light dark:bg-orange-dark text-primary-three dark:text-secondary-three rounded-xl p-3.5">
+    <div className="w-full md:w-8/12 flex items-center justify-center gap-3.5 md:gap-7 py-5 text-xs md:text-xl">
+      <button className="w-full max-w-[200px] md:max-w-none flex gap-2 items-center justify-center bg-orange-light dark:bg-orange-dark text-primary-three dark:text-secondary-three rounded-xl p-3.5 md:p-5">
         Download
-        <img className="w-4 h-4 object-cover" src="/download.svg" alt="download-icon" />
+        <img className="w-4 h-4 md:w-6 md:h-6 object-cover" src="/download.svg" alt="download-icon" />
       </button>
-      <button className="flex-1 bg-switch-light dark:bg-switch-dark text-primary-three dark:text-secondary-two rounded-xl p-3.5">
+      <button className="w-full max-w-[200px] md:max-w-none bg-switch-light dark:bg-switch-dark text-primary-three dark:text-secondary-two rounded-xl p-3.5 md:p-5">
         Contact
       </button>
     </div>
