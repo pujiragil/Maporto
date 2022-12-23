a6ece5e172a9ba72a13da1d575aa64e506c079c2
pujiragil <ragilsg164@gmail.com>
2022-12-23 07:21:01 +0700

    custom button size

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,6 +1,6 @@
 export default function MainButton() {
   return (
-    <div className="w-full md:w-7/12 lg:w-5/12 flex items-center justify-center gap-3.5 md:gap-7 py-2 md:py-2.5 text-xs md:text-sm lg:text-base">
+    <div className="w-full md:w-8/12 lg:w-6/12 flex items-center justify-center gap-3.5 md:gap-7 py-2 md:py-2.5 text-xs md:text-sm lg:text-base">
       <button className="primary-button">
         Download
         <img className="w-4 h-4 md:w-5 md:h-5 object-cover" src="/download.svg" alt="download-icon" />
