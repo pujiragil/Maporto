ee575efe950e8ea64dfa4ad361bb40f5e77e029a
pujiragil <ragilsg164@gmail.com>
2022-12-23 09:13:53 +0700

    custom source button

--- components/Portfolio/index.tsx
+++ components/Portfolio/index.tsx
@@ -28,7 +28,7 @@ function PortfolioItem({ src, alt }: PortfolioItemProps) {
         alt={alt}
       />
       <div className="absolute flex items-center justify-center gap-4 bg-switch-light/70 dark:bg-switch-dark/70 w-full h-1/2 bottom-0 left-0 translate-y-full transition-all duration-300 ease-in-out group-hover:translate-y-0 text-xs md:text-sm lg:text-base">
-        <button className="w-1/3 p-3 bg-switch-light dark:bg-switch-dark rounded-xl flex items-center justify-center gap-2"><DiGithubBadge className="text-xl md:text-2xl"/> Source</button>
+        <button className="w-1/3 p-3 bg-switch-light/80 hover:bg-switch-light dark:bg-switch-dark text-primary-two dark:text-secondary-two hover:text-primary-four hover:dark:text-secondary-one rounded-xl flex items-center justify-center gap-2 transition duration-300 ease-in-out"><DiGithubBadge className="text-xl md:text-2xl"/> Source</button>
         <button className="w-1/3 p-3 bg-orange-light dark:bg-orange-dark rounded-xl flex items-center justify-center gap-2"><HiOutlineGlobeEuropeAfrica className="text-xl md:text-2xl"/> Live</button>
       </div>
     </div>
