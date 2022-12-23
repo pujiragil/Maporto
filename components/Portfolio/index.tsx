94950d972def63e9c44a9a005bbab20c46d3b1d7
pujiragil <ragilsg164@gmail.com>
2022-12-23 09:46:29 +0700

    apply custom portfolio button

--- components/Portfolio/index.tsx
+++ components/Portfolio/index.tsx
@@ -1,5 +1,5 @@
-import { DiGithubBadge } from "react-icons/di"
-import { HiOutlineGlobeEuropeAfrica } from "react-icons/hi2"
+import { DiGithubBadge } from "react-icons/di";
+import { HiOutlineGlobeEuropeAfrica } from "react-icons/hi2";
 
 export default function Portfolio() {
   return (
@@ -27,9 +27,13 @@ function PortfolioItem({ src, alt }: PortfolioItemProps) {
         src={src}
         alt={alt}
       />
-      <div className="absolute flex items-center justify-center gap-4 bg-switch-light/70 dark:bg-switch-dark/70 w-full h-1/2 bottom-0 left-0 translate-y-full transition-all duration-300 ease-in-out group-hover:translate-y-0 text-xs md:text-sm lg:text-base">
-        <button className="w-1/3 p-3 bg-switch-light/80 hover:bg-switch-light dark:bg-switch-dark text-primary-two dark:text-secondary-two hover:text-primary-four hover:dark:text-secondary-one rounded-xl flex items-center justify-center gap-2 transition duration-300 ease-in-out"><DiGithubBadge className="text-xl md:text-2xl"/> Source</button>
-        <button className="w-1/3 p-3 bg-orange-light dark:bg-orange-dark rounded-xl flex items-center justify-center gap-2"><HiOutlineGlobeEuropeAfrica className="text-xl md:text-2xl"/> Live</button>
+      <div className="portfolio-button-wrapper">
+        <button className="portfolio-button-primary">
+          <DiGithubBadge className="text-xl md:text-2xl" /> Source
+        </button>
+        <button className="portfolio-button-secondary">
+          <HiOutlineGlobeEuropeAfrica className="text-xl md:text-2xl" /> Live
+        </button>
       </div>
     </div>
   );
