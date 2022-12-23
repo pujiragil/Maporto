c68986ed1d4848330ffbd93c0641ddf6cd0624f5
pujiragil <ragilsg164@gmail.com>
2022-12-23 13:28:35 +0700

    change img avatar tag with next/image

--- components/Avatar/index.tsx
+++ components/Avatar/index.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
+import avatar from "/public/images/avatar.jpg"
+
 export default function Avatar() {
   return (
     <div className="p-1.5 border-4 border-orange-dark rounded-full w-fit cursor-pointer avatar">
-      <img className="w-28 h-28 md:w-36 md:h-36 lg:w-48 lg:h-48 object-cover rounded-full grayscale" src="/images/avatar.jpg" alt="avatar-profile" />
+      <Image className="w-28 h-28 md:w-36 md:h-36 lg:w-48 lg:h-48 object-cover rounded-full grayscale" src={avatar} alt="avatar-profile" />
     </div>
   );
 }
